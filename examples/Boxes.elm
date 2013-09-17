41b8e2b6e7b5fe5388fc1506631e1c91a2bec873
Spiros Eliopoulos <seliopou@gmail.com>
2013-09-17 01:35:46 -0700

    examples: boxes

---
+++ examples/Boxes.elm
@@ -0,0 +1,43 @@
+module Boxes where
+
+import open D3
+import Mouse
+
+size   = 300
+margin = { top = 10, left = 10, right = 10, bottom = 10 }
+dims   = { height = size - margin.top - margin.bottom
+         , width  = size - margin.left - margin.right }
+
+type Dimensions = { height : number, width : number }
+type Margins = { top : number, left : number, right : number, bottom : number }
+
+svg : Dimensions -> Margins -> Selection a
+svg ds ms =
+  append "svg"
+  |. num attr "height" (ds.height + ms.top + ms.bottom)
+  |. num attr "width"  (ds.width  + ms.left + ms.right)
+  |. append "g"
+     |. str attr "transform" (translate margin.left margin.top)
+
+boxes : Selection (number, number)
+boxes = 
+  selectAll ".box"
+  |. bind (\(x, y) -> [(x, 0, "cyan"), (0, y, "magenta")])
+     -- enter
+     (append "rect"
+     |. str attr "class" "box"
+     |. num attr "width"  100
+     |. num attr "height" 100
+     |. attr     "fill"   (\(_, _, c) _ -> c))
+     -- update
+     (update
+     |. attr "x" (\(x, _, _) _ -> show x)
+     |. attr "y" (\(_, y, _) _ -> show y))
+     -- exit
+     remove
+
+translate : number -> number -> String
+translate x y = "translate(" ++ (show x) ++ "," ++ (show y) ++ ")"
+
+main : Signal Element
+main = render dims.height dims.width (svg dims margin) boxes <~ Mouse.position
