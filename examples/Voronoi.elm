781041fc99a66fc84ff3591ec51fe6beff8e99d9
Spiros Eliopoulos <seliopou@gmail.com>
2014-06-22 17:18:55 -0400

    examples: Voronoi documentation

--- examples/Voronoi.elm
+++ examples/Voronoi.elm
@@ -1,19 +1,50 @@
+-- This is an Elm reimplementation of a popular Voronoi diagram example using
+-- elm-d3. The original can be found here:
+--
+--   http://bl.ocks.org/mbostock/4060366
+--
+-- From the root directory of the elm-d3 project, compile it using the
+-- following commands:
+--
+--   make
+--   elm --make -src-dir=src `./scripts/build-flags` `./scripts/include-css examples/voronoi.css` examples/Voronoi.elm
+--
+-- On OS X, you can then open the file in the browser using the following
+-- command:
+--
+--   open build/examples/Voronoi.html
+--
 module Voronoi where
 
+-- Import D3 and dump all its names into the current scope. Also import Voronoi
+-- diagram-related operations, but keep those names qualified.
+--
 import D3(..)
 import D3.Voronoi
+
+-- Import Mouse to access the mouse's position. Also import Random and dump its
+-- names into the current scope.
+--
 import Mouse
 import Random(..)
 
+-- Type declaractions for records that represent dimensions and margins.
+--
+type Dimensions = { height : Float, width : Float }
+type Margins = { top : Float, left : Float, right : Float, bottom : Float }
+
+
 width = 960
 height = 500
-margin = { top = 0, left = 0, right = 0, bottom = 0 }
 
+margin = { top = 0, left = 0, right = 0, bottom = 0 }
 dims   = { height = height - margin.top - margin.bottom
          , width  = width - margin.left - margin.right }
 
-type Dimensions = { height : Float, width : Float }
-type Margins = { top : Float, left : Float, right : Float, bottom : Float }
+-- A function that will create an <svg> element with a nested <g> element. The
+-- function will use the given dimensions and margins the set the width and
+-- height of the <svg> element.
+--
 
 svg : Dimensions -> Margins -> Selection a
 svg ds ms =
@@ -46,14 +77,22 @@ voronoi =
 cells : [D3.Voronoi.Point] -> [[D3.Voronoi.Point]]
 cells = D3.Voronoi.cellsWithClipping margin.right margin.top dims.width dims.height
 
+-- Helper function for creating an SVG path string for the given polygon.
+--
 path : [D3.Voronoi.Point] -> String
 path ps =
   let pair p = (show p.x) ++ "," ++ (show p.y)
     in "M" ++ (join "L" (map pair ps)) ++ "Z"
 
+-- Helper function for creating an SVG transformation string that represents a
+-- translation.
+--
 translate : number -> number -> String
 translate x y = "translate(" ++ (show x) ++ "," ++ (show y) ++ ")"
 
+-- Generates a list of random points of the given length. The list is returned
+-- as a Signal, thought the signal will never take on any other value.
+--
 randomPoints : Int -> Signal [D3.Voronoi.Point]
 randomPoints n =
   let mk_point x y = { x = x * dims.width , y = y * dims.height } in
