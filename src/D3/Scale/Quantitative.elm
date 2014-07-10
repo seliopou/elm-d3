00815386259975859587b3cf60673c50e60cc8fd
Michael James <michael@michaelbjames.com>
2014-07-11 00:52:27 +0200

    @seliopou's first round of requested fixes (namechange + dead code removal + clamp tweak)

--- src/D3/Scale/Quantitative.elm
+++ src/D3/Scale/Quantitative.elm
@@ -1,4 +1,4 @@
-module D3.Scales.Quantitative where
+module D3.Scale.Quantitative where
 {-| Quantitative Scales in elm-d3. These are for scales that take a value
 from the Reals and map it to the Reals
 
@@ -11,31 +11,31 @@ from the Reals and map it to the Reals
 # Do things with a scale
 @docs convert, invert, ticks
 -}
-import Native.D3.Scales.Quantitative
+import Native.D3.Scale.Quantitative
 
 data Scale a = Scale
 {-| Make a linear scale. Nothing fancy here. The default is the identity mapping.
 -}
 linear : Scale Float
-linear = Native.D3.Scales.Quantitative.linear
+linear = Native.D3.Scale.Quantitative.linear
 
 identity : Scale Float
-identity = Native.D3.Scales.Quantitative.identity
+identity = Native.D3.Scale.Quantitative.identity
 
 {-| Make a sqrt scale.
 -}
 sqrt : Scale Float
-sqrt = Native.D3.Scales.Quantitative.sqrt
+sqrt = Native.D3.Scale.Quantitative.sqrt
 
 {-| Make a power scale. You must specify the exponent.
 -}
 pow : Float -> Scale Float
-pow = Native.D3.Scales.Quantitative.pow
+pow = Native.D3.Scale.Quantitative.pow
 
 {-| Make a log scale. You must specify the base.
 -}
 log : Float -> Scale Float
-log = Native.D3.Scales.Quantitative.log
+log = Native.D3.Scale.Quantitative.log
 
 
 {-| Change the domain of the function. You may pass in any number of values into
@@ -44,28 +44,28 @@ the list and they will be interpolated between.
       yScale = linear |> domain [0,100] |> range [-150,150]
 -}
 domain : [Float] -> Scale a -> Scale a
-domain = Native.D3.Scales.Quantitative.domain
+domain = Native.D3.Scale.Quantitative.domain
 
 {-| Change the range of the function. You may pass in any number of values into
 the list and they will be interpolated between.
 -}
 range : [a] -> Scale a -> Scale a
-range = Native.D3.Scales.Quantitative.range
+range = Native.D3.Scale.Quantitative.range
 
 {-| Extend the domain to start and end on "nice" values.
 
       linear |> domain [0.201, 0.934] |> nice == linear |> domain [0.2, 1]
 -}
 nice : Scale Float -> Scale Float
-nice = Native.D3.Scales.Quantitative.nice
+nice = Native.D3.Scale.Quantitative.nice
 
 {-| Clamp the domain so that values beyond the domain will be brough back to the
 most extreme value specified in the domain.
 
-      linear |> clamp |> convert 3 == 1
+      linear |> clamp True |> convert 3 == 1
 -}
-clamp : Scale Float -> Scale Float
-clamp = Native.D3.Scales.Quantitative.clamp
+clamp : Bool -> Scale Float -> Scale Float
+clamp = Native.D3.Scale.Quantitative.clamp
 
 
 {-| Apply the scale to a value and it will map the input value to the output range.
@@ -73,17 +73,17 @@ clamp = Native.D3.Scales.Quantitative.clamp
       linear |> domain [0,1] |> range [0,100] |> convert 0.5 == 50
 -}
 convert : Float -> Scale b -> b
-convert = Native.D3.Scales.Quantitative.convert
+convert = Native.D3.Scale.Quantitative.convert
 
 {-| Apply the scale to a value in its range to get the corresponding value in
 the domain.
 -}
 invert : Float -> Scale Float -> Float
-invert = Native.D3.Scales.Quantitative.invert
+invert = Native.D3.Scale.Quantitative.invert
 
 {-| Get a list of where the ticks are on the scale, given a number of ticks you want.
 
       linear |> domain [0,10] |> ticks 3 == [0, 5, 10]
 -}
 ticks : Int -> Scale Float -> [Float]
-ticks = Native.D3.Scales.Quantitative.ticks
+ticks = Native.D3.Scale.Quantitative.ticks
---
+++ src/Native/D3/Scale/Quantitative.js
@@ -0,0 +1,72 @@
+Elm.Native.D3.Scale.Quantitative = {};
+Elm.Native.D3.Scale.Quantitative.make = function (elm) {
+    'use strict';
+
+    elm.Native = elm.Native || {};
+    elm.Native.D3 = elm.Native.D3 || {};
+    elm.Native.D3.Scale = elm.Native.D3.Scale || {};
+    elm.Native.D3.Scale.Quantitative = elm.Native.D3.Scale.Quantitative || {};
+    if (elm.Native.D3.Scale.Quantitative.values) return elm.Native.D3.Scale.Quantitative.values;
+
+    var JS = Elm.Native.D3.JavaScript.make(elm);
+
+    function pow(exponent) {
+        return d3.scale.pow().exponent(JS.toFloat(exponent));
+    }
+
+    function log(base) {
+        return d3.scale.log().base(JS.toFloat(base));
+    }
+
+    function domain(domArray, scale) {
+        var newScale = scale.copy();
+        var newDomain = JS.fromList(domArray);
+        return newScale.domain(newDomain);
+    }
+
+    function range(rangeArray, scale) {
+        var newScale = scale.copy();
+        var newRange = JS.fromList(rangeArray);
+        return newScale.range(newRange);
+    }
+
+    function ticks(nTicks, scale) {
+        var tempScale = scale.copy();
+        return tempScale.ticks(JS.toInt(nTicks));
+    }
+
+    function nice(scale) {
+        var newScale = scale.copy();
+        return newScale.nice();
+    }
+
+    function clamp(bool, scale) {
+        var newScale = scale.copy();
+        return newScale.clamp(JS.toBool(bool));
+    }
+
+    function convert(n, scale) {
+        var number = JS.toFloat(n);
+        return scale(number);
+    }
+
+    function invert(n, scale) {
+        var number = JS.toFloat(n);
+        return scale.invert(number);
+    }
+
+    return elm.Native.D3.Scale.Quantitative.values =
+        { linear : d3.scale.linear()
+        , identity : d3.scale.identity()
+        , sqrt : d3.scale.sqrt()
+        , pow : pow
+        , log : log
+        , nice : nice
+        , clamp : clamp
+        , domain : F2(domain)
+        , range : F2(range)
+        , ticks : F2(ticks)
+        , convert : F2(convert)
+        , invert : F2(invert)
+        };
+};
